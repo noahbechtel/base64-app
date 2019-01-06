4bdc69021f89b0a2835d4dc2e88f8e274fe88ef5
Noah <noahbehtel@gmail.com>
2019-01-05 21:11:28 -0600

    minor spelling error

--- client/components/about.js
+++ client/components/about.js
@@ -11,8 +11,8 @@ const About = () => (
       <h3>
         Base64 is a somewhat simple encoding that allows transfer of binary data
         over mediums that can only parse printable characters. One of the first
-        implementations was to allows computers with the same OS communicate
-        over a dial-up connection. Today it is still heavily used in E-mail MIME
+        implementations was to allow computers with the same OS communicate over
+        a dial-up connection. Today it is still heavily used in E-mail MIME
         protocol and when transferring binary data across networks. However, a
         new use for it has recently emerged across Reddit; a way to encode links
         to prevent them from being mapped and removed by bot moderators and nosy
